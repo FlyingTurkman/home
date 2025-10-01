7e1746cbb68d7befd4b5767f292551fdc658445c
Sharkman <dytdenizarli@hotmail.com>
2025-10-01 18:37:07 +0300

    cors applied
    cors applied for cominucation with cart

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -11,6 +11,8 @@ import { addToBasket } from "@/actions/addToBasket"
 import { useSiteContext } from "@/context/SiteContextProvider"
 import { toast } from "sonner"
 import { FaShoppingCart } from "react-icons/fa"
+import { IoAdd, IoRemove } from "react-icons/io5"
+import { useState } from "react"
 
 
 
@@ -30,6 +32,7 @@ export default function ProductCard({
     product: productType
 }) {
 
+    const [count, setCount] = useState<number>(1)
 
     const { setCart } = useSiteContext()
 
@@ -85,28 +88,58 @@ export default function ProductCard({
                 </div>
             </CardContent>
             <CardFooter>
-                <Button
-                className="w-full"
-                onClick={ async () => {
-                    const response = await addToBasket({
-                        ...product,
-                        count: 1
-                    })
-
-                    if (response.status) {
-                        toast.success('İşlem başarılı', {
-                            description: response.message
-                        })
-                    } else {
-                        toast.error('İşlem başarısız.', {
-                            description: response.message
-                        })
-                    }
-                }}
+                <div
+                className="flex flex-col gap-2 w-full"
                 >
-                    <FaShoppingCart/>
-                    Sepete Ekle
-                </Button>
+                    <div
+                    className="flex flex-row items-center justify-between"
+                    >
+                        <Button
+                        variant={'outline'}
+                        disabled={count == 1}
+                        onClick={() => {
+                            setCount((prev) => prev == 1 ? 1 : prev - 1)
+                        }}
+                        >
+                            <IoRemove/>
+                        </Button>
+                        <Label
+                        className="text-xl"
+                        >
+                            {count}
+                        </Label>
+                        <Button
+                        variant={'outline'}
+                        onClick={() => {
+                            setCount((prev) => prev + 1)
+                        }}
+                        >
+                            <IoAdd/>
+                        </Button>
+                    </div>
+                    <Button
+                    className="w-full"
+                    onClick={ async () => {
+                        const response = await addToBasket({
+                            ...product,
+                            count
+                        })
+
+                        if (response.status) {
+                            toast.success('İşlem başarılı', {
+                                description: response.message
+                            })
+                        } else {
+                            toast.error('İşlem başarısız.', {
+                                description: response.message
+                            })
+                        }
+                    }}
+                    >
+                        <FaShoppingCart/>
+                        Sepete Ekle
+                    </Button>
+                </div>
             </CardFooter>
         </Card>
     )
